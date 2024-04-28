50636489d72202ebf56fe620faebfc95a9128925
Fardin15 <mohammedshowrav15@gmail.com>
2024-04-28 21:41:43 +0600

    data get from database bases on user

--- src/Pages/List.jsx
+++ src/Pages/List.jsx
@@ -1,7 +1,23 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { data } from "autoprefixer";
+
 const List = () => {
+  const { user } = useContext(AuthContext);
+  const [spots, setSpots] = useState([]);
+
+  const email = user.email;
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/mylist/${email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSpots(data);
+      });
+  }, [user]);
   return (
     <div>
-      <h1>my list</h1>
+      <h1>my list{spots.length}</h1>
     </div>
   );
 };
